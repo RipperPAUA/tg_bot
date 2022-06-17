a9b4347b3da91f34084fc47d57ecfbf4a8dd24a7
RipperPAUA <passhhappv1997@gmail.com>
2022-06-17 13:18:14 +0300

    Refactoring 3

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import logging
 import os
 from aiogram.dispatcher import Dispatcher
-from aiogram.utils  import executor
+from aiogram.utils import executor
 from aiogram import Bot, types
-
 
 TOKEN = os.getenv('BOT_TOKEN')
 bot = Bot(token=TOKEN)
@@ -16,13 +15,18 @@ WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
 WEBHOOK_PATH = f'/webhook/{TOKEN}'
 WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 
+logging.warning(f"TOKEN: {TOKEN}")
+logging.warning(f"HEROKU_APP_NAME: {HEROKU_APP_NAME}")
+logging.warning(f"WEBHOOK_HOST: {WEBHOOK_HOST}")
+logging.warning(f"WEBHOOK_PATH: {WEBHOOK_PATH}")
+logging.warning(f"WEBHOOK_URL: {WEBHOOK_URL}")
+
 # webserver settings
 WEBAPP_HOST = '0.0.0.0'
 WEBAPP_PORT = os.getenv('PORT', default=8000)
 
-
 async def on_startup(dispatcher):
-    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
+    await bot.set_webhook(WEBHOOK_URL, )
 
 @dp.message_handler()
 async def echo(message: types.Message):
@@ -36,7 +40,6 @@ if __name__ == '__main__':
     executor.start_webhook(
         dispatcher=dp,
         webhook_path=WEBHOOK_PATH,
-        skip_updates=True,
         on_startup=on_startup,
         on_shutdown=on_shutdown,
         host=WEBAPP_HOST,
