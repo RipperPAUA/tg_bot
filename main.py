90b31b3b39daf627cc70af6cbb4437974c070a2e
RipperPAUA <passhhappv1997@gmail.com>
2022-06-19 20:46:19 +0300

    Restart project

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import logging
 import os
+from aiogram import Bot
 from aiogram.dispatcher import Dispatcher
-from aiogram.utils import executor
+from aiogram.utils.executor import start_webhook
 from aiogram import Bot, types
+
 
 TOKEN = os.getenv('BOT_TOKEN')
 bot = Bot(token=TOKEN)
@@ -12,34 +14,30 @@ HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
 
 # webhook settings
 WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
-WEBHOOK_PATH = "/webhook"
+WEBHOOK_PATH = f'/webhook/{TOKEN}'
 WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
-
-logging.warning(f"TOKEN: {TOKEN}")
-logging.warning(f"HEROKU_APP_NAME: {HEROKU_APP_NAME}")
-logging.warning(f"WEBHOOK_HOST: {WEBHOOK_HOST}")
-logging.warning(f"WEBHOOK_PATH: {WEBHOOK_PATH}")
-logging.warning(f"WEBHOOK_URL: {WEBHOOK_URL}")
 
 # webserver settings
 WEBAPP_HOST = '0.0.0.0'
 WEBAPP_PORT = os.getenv('PORT', default=8000)
 
-async def on_startup(dispatcher: Dispatcher):
-    await bot.set_webhook(WEBHOOK_URL)
+
+async def on_startup(dispatcher):
+    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
 
 @dp.message_handler()
 async def echo(message: types.Message):
     await message.answer(message.text)
 
-async def on_shutdown(dispatcher: Dispatcher):
+async def on_shutdown(dispatcher):
     await bot.delete_webhook()
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-    executor.start_webhook(
+    start_webhook(
         dispatcher=dp,
         webhook_path=WEBHOOK_PATH,
+        skip_updates=True,
         on_startup=on_startup,
         on_shutdown=on_shutdown,
         host=WEBAPP_HOST,
