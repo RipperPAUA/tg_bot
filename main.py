5567035b8befa47c929e9cd8d982a61b53bf6b74
RipperPAUA <passhhappv1997@gmail.com>
2022-06-21 19:33:28 +0300

    fix 4

--- main.py
+++ main.py
@@ -1,45 +1,66 @@
 import logging
-import os
-from aiogram import Bot
-from aiogram.dispatcher import Dispatcher
-from aiogram.utils.executor import start_webhook
+
 from aiogram import Bot, types
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
+from aiogram.dispatcher import Dispatcher
+from aiogram.dispatcher.webhook import SendMessage
+from aiogram.utils.executor import start_webhook
 
 
-TOKEN = os.getenv('BOT_TOKEN')
-bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
-
-HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
+API_TOKEN = 'BOT_TOKEN_HERE'
 
 # webhook settings
-WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
-WEBHOOK_PATH = f'/webhook/{TOKEN}'
-WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+WEBHOOK_HOST = 'https://ripperpaua-tg-bot.herokuapp.com'
+WEBHOOK_PATH = '/webhook'
+WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
 
 # webserver settings
-WEBAPP_HOST = '0.0.0.0'
-WEBAPP_PORT = os.getenv('PORT', default=8000)
+WEBAPP_HOST = 'localhost'  # or ip
+WEBAPP_PORT = 3001
 
+logging.basicConfig(level=logging.INFO)
 
-async def on_startup(dispatcher):
-    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+dp.middleware.setup(LoggingMiddleware())
+
 
 @dp.message_handler()
 async def echo(message: types.Message):
-    await message.answer(message.text)
+    # Regular request
+    # await bot.send_message(message.chat.id, message.text)
 
-async def on_shutdown(dispatcher):
+    # or reply INTO webhook
+    return SendMessage(message.chat.id, message.text)
+
+
+async def on_startup(dp):
+    await bot.set_webhook(WEBHOOK_URL)
+    # insert code here to run it after start
+
+
+async def on_shutdown(dp):
+    logging.warning('Shutting down..')
+
+    # insert code here to run it before shutdown
+
+    # Remove webhook (not acceptable in some cases)
     await bot.delete_webhook()
 
+    # Close DB connection (if used)
+    await dp.storage.close()
+    await dp.storage.wait_closed()
+
+    logging.warning('Bye!')
+
+
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
     start_webhook(
         dispatcher=dp,
         webhook_path=WEBHOOK_PATH,
-        skip_updates=True,
         on_startup=on_startup,
         on_shutdown=on_shutdown,
+        skip_updates=True,
         host=WEBAPP_HOST,
         port=WEBAPP_PORT,
     )
