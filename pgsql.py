511d8ebf7d0f293c84b18d33ae408ea651c4a604
RipperPAUA <passhhappv1997@gmail.com>
2022-07-01 00:57:40 +0300

    upd pgsql.py

--- pgsql.py
+++ pgsql.py
@@ -4,8 +4,18 @@ import environ
 env=environ.Env()
 env.read_env(".env")
 
-
-conn = psycopg2.connect(dbname=env("DB_NAME"), user=env("DB_USER"),
+try:
+    conn = psycopg2.connect(dbname=env("DB_NAME"), user=env("DB_USER"),
                         password=env("DB_PASSWORD"), host=env("DB_HOST"),
                         )
-cursor = conn.cursor()
+    # cursor = conn.cursor()
+
+    with conn.cursor() as cursor:
+        pass
+
+except:
+    print("[INFO] Error while connecting to database")
+finally:
+    if conn:
+        conn.close()
+        print("[INFO] Connection closed to database")
