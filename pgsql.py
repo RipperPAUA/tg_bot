7d2c3903cb37f78d42dd145262ff69fff00f52af
RipperPAUA <passhhappv1997@gmail.com>
2022-07-01 02:40:46 +0300

    upd state pgsql.py

---
+++ pgsql.py
@@ -0,0 +1,29 @@
+import psycopg2
+import environ
+import logging
+
+env=environ.Env()
+env.read_env(".env")
+
+# connection to database
+try:
+    conn = psycopg2.connect(
+        host=env("DB_HOST"),
+        user=env("DB_USER"),
+        password=env("DB_PASSWORD"),
+        database=env("DB_NAME")
+    )
+    # the cursor for performing database operations
+    # cursor = conn.cursor()
+
+    with conn.cursor() as cursor:
+        pass
+
+
+except:
+    logging.warning("Error while working with Postgree")
+finally:
+    if conn:
+        # cursor.close()
+        conn.close()
+        logging.warning("PostgreeSQL connection closed")
