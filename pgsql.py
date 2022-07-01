67b482c4f45658b4f4f80f262663553205a75f1d
RipperPAUA <passhhappv1997@gmail.com>
2022-07-01 03:25:04 +0300

    making changes to the pgsql.py file

--- pgsql.py
+++ pgsql.py
@@ -1,29 +1,32 @@
-import psycopg2
-import environ
+from main import conn
 import logging
 
-env=environ.Env()
-env.read_env(".env")
-
-# connection to database
-try:
-    conn = psycopg2.connect(
-        host=env("DB_HOST"),
-        user=env("DB_USER"),
-        password=env("DB_PASSWORD"),
-        database=env("DB_NAME")
+with conn.cursor() as cursor:
+    cursor.execute(
+        "SELECT version();"
     )
-    # the cursor for performing database operations
-    # cursor = conn.cursor()
-
-    with conn.cursor() as cursor:
-        pass
-
-
-except:
-    logging.warning("Error while working with Postgree")
-finally:
-    if conn:
-        # cursor.close()
-        conn.close()
-        logging.warning("PostgreeSQL connection closed")
+    logging.warning(f"Server version: {cursor.fetchone()}")
+    # print(f"Server version: {cursor.fetchone()}")
+#
+# # connection to database
+# try:
+#     conn = psycopg2.connect(
+#         host=env("DB_HOST"),
+#         user=env("DB_USER"),
+#         password=env("DB_PASSWORD"),
+#         database=env("DB_NAME")
+#     )
+#     # the cursor for performing database operations
+#     # cursor = conn.cursor()
+#
+#     with conn.cursor() as cursor:
+#         pass
+#
+#
+# except:
+#     logging.warning("Error while working with Postgree")
+# finally:
+#     if conn:
+#         # cursor.close()
+#         conn.close()
+#         logging.warning("PostgreeSQL connection closed")
