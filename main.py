2204e8ee8fa24355c659187a462b31f9b9ca221d
RipperPAUA <passhhappv1997@gmail.com>
2022-07-05 22:27:59 +0300

    upd main.py(2)

--- main.py
+++ main.py
@@ -3,32 +3,25 @@ import logging
 from aiogram.utils.executor import start_webhook
 from create_bot import dp, bot, env, WEBHOOK_URL, WEBHOOK_PATH, WEBAPP_HOST, WEBAPP_PORT
 
-try:
-    conn=psycopg2.connect(
-        host=env("DB_HOST"),
-        user=env("DB_USER"),
-        password=env("DB_PASSWORD"),
-        database=env("DB_NAME")
-    )
-except Exception as _ex:
-    conn.close()
-    logging.warning(_ex, "Error starting connection")
-
+conn = psycopg2.connect(
+    host=env("DB_HOST"),
+    user=env("DB_USER"),
+    password=env("DB_PASSWORD"),
+    database=env("DB_NAME")
+)
 
 async def on_startup(dp):
     logging.warning('Starting.....')
     logging.warning(f"{WEBHOOK_URL}")
     await bot.set_webhook(WEBHOOK_URL)
 
-
-# insert code here to run it after start
-
+from handlers import other
+other.register_handlers_other(dp)
 
 async def on_shutdown(dp):
     logging.warning('Shutting down..')
     await bot.delete_webhook()
     logging.warning('Bye!')
-    conn.execute()
 
 if __name__ == '__main__':
     start_webhook(
