cc8aa324f0aafc19d1a8fa7c3df1120eb8f09fbe
RipperPAUA <passhhappv1997@gmail.com>
2022-07-06 00:09:16 +0300

    correct connection to db in on_startup and on_shutdown

--- main.py
+++ main.py
@@ -4,39 +4,45 @@ from aiogram.utils.executor import start_webhook
 from create_bot import dp, bot, env, WEBHOOK_URL, WEBHOOK_PATH, WEBAPP_HOST, WEBAPP_PORT
 
 
-async def on_startup(dp, conn):
+async def on_startup(dp):
     logging.warning('Starting.....')
     logging.warning(f"{WEBHOOK_URL}")
     await bot.set_webhook(WEBHOOK_URL)
-    # try:
-    #     conn = psycopg2.connect(
-    #         host=env("DB_HOST"),
-    #         user=env("DB_USER"),
-    #         password=env("DB_PASSWORD"),
-    #         database=env("DB_NAME")
-    #     )
-    #     logging.warning("Connecting to PostgreSQL success")
-    # except:
-    #     logging.warning("Starting connection is falled")
-    #     conn.close()
-    #     return conn
+    try:
+        logging.warning("Starting connection")
+        conn = psycopg2.connect(
+            host=env("DB_HOST"),
+            user=env("DB_USER"),
+            password=env("DB_PASSWORD"),
+            database=env("DB_NAME")
+        )
+    # the cursor for performing database operations
+    # cursor = conn.cursor()
 
+    except Exception as _ex:
+        logging.warning(_ex, "Error starting connection")
+
+    return conn
 
 from handlers import other
-
 other.register_handlers_other(dp)
 
 
-async def on_shutdown(dp, conn):
+async def on_shutdown(dp,conn):
+    # insert code here to run it before shutdown
+    try:
+        if conn:
+            # cursor.close()
+            conn.close()
+            logging.warning("PostgreeSQL connection closed")
+    except:
+        conn.close()
+        logging.warning("Error closing connection database")
+
     logging.warning('Shutting down..')
     await bot.delete_webhook()
     logging.warning('Bye!')
-    # try:
-    #     conn.close()
-    #     logging.warning("Connection is closed")
-    # except:
-    #     logging.warning("Connection was closed failld")
-    #     conn.close()
+    return conn
 
 
 if __name__ == '__main__':
